223ec415400f8bcc5b5427d4887ea8a3e510c23d
Rizwan Zia <rizwanzia18@gmail.com>
2020-08-06 16:27:52 +0530

    initial commit.

---
+++ app/src/main/java/com/example/counter/ImageAdapter.java
@@ -0,0 +1,18 @@
+package com.example.counter;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+public class ImageAdapter extends PagerAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return false;
+    }
+}
